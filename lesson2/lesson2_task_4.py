f5dc5f21845f2aac01dd500654ee85746d7b83cf
Natalya <zetta2828p@gmail.com>
2025-05-07 20:43:11 +0300

    lesson2

---
+++ lesson2/lesson2_task_4.py
@@ -0,0 +1,20 @@
+# Напишем функцию fizz_buzz, которая принимает один аргумент (число n):
+# Если число делится на 3, печатать Fizz.
+# Если число делится на 5, печатать Buzz.
+# Если число делится и на 3 и на 5, печатать FizzBuzz
+
+num = int(input("Введите число: "))
+
+def fizz_buzz(n):
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+fizz_buzz(num)
